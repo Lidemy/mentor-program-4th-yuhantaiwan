cf1c5dfbc09a6791101d138942a5bd79a7abceec
禦寒 <taiwansmile@gmail.com>
2020-07-03 01:43:49 +0800

    first commit week2 hw

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,15 @@
 function capitalize(str) {
-  
+  if (str[0] >= 'A' && str[0] <= 'Z') {
+    return str
+  } else if (str[0] >= 'a' && str[0] <= 'z') {
+    let char = String.fromCharCode(str.charCodeAt(0) - ('a'.charCodeAt(0) - 'A'.charCodeAt(0)))
+    str =  str.replace(str[0], char)
+    return str
+  } else {
+    return str
+  }
 }
 
+console.log(capitalize('Hello'));
 console.log(capitalize('hello'));
+console.log(capitalize(',hello'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,23 @@
 function join(arr, concatStr) {
-  
+  let str = '';
+  for(let i=0; i<arr.length; i++) {
+    str = str + arr[i].toString() + concatStr;
+  }
+  str = str.slice(0,-1);
+  return str
 }
 
 function repeat(str, times) {
-  
+  let reStr = '';
+  for(let i=1; i<=times; i++) {
+    reStr = reStr + str;
+  }
+  return reStr
 }
 
 console.log(join(['a'], '!'));
+console.log(join(["a", "b", "c"], "!"));
+console.log(join(["a", 1, "b", 2, "c", 3], ','));
+
 console.log(repeat('a', 5));
+console.log(repeat('yoyo', 2));
