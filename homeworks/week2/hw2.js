5216ec1f2799d70c5d6f22ef55b4d61c2d44e6a2
禦寒 <taiwansmile@gmail.com>
2020-07-03 11:01:08 +0800

    second commit

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,7 +1,7 @@
 function capitalize(str) {
   if (str[0] >= 'A' && str[0] <= 'Z') {
     return str
-  } else if (str[0] >= 'a' && str[0] <= 'z') {
+  } else if (str[0] >= 'a' && str[0] <= 'z') {  // 把第一個小寫字母轉大寫
     let char = String.fromCharCode(str.charCodeAt(0) - ('a'.charCodeAt(0) - 'A'.charCodeAt(0)))
     str =  str.replace(str[0], char)
     return str
