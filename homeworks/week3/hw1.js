964bc5534d6ea76756554643d8b1abc947da3ab3
禦寒 <taiwansmile@gmail.com>
2020-07-14 12:15:04 +0800

    week3 hw1 eslint modifiy

---
+++ homeworks/week3/hw1.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-use-before-define, no-shadow */
+const readline = require('readline');
+
+const lines = [];
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+
+rl.on('line', (line) => {
+  lines.push(line);
+});
+
+rl.on('close', () => {
+  solve(lines);
+});
+
+function solve(lines) {
+  const n = lines[0];
+  printStars(n);
+}
+
+function printStars(n) {
+  let star = '';
+  for (let i = 0; i < n; i += 1) {
+    star += '*';
+    console.log(star);
+  }
+}
